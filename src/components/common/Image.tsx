261ead2272397ae3cb886a132cd38ab890c1065d
Mikita Kavaliou <nik.kow@outlook.com>
2024-12-22 09:55:42 +0100

    create image componment

---
+++ src/components/common/Image.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+interface ImageProps {
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+  className?: string;
+  priority?: boolean;
+}
+
+const Image: React.FC<ImageProps> = ({
+  src,
+  alt,
+  width,
+  height,
+  className = '',
+  priority = false,
+}) => {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      className={className}
+      loading={priority ? 'eager' : 'lazy'}
+    />
+  );
+};
+
+export default Image; 
